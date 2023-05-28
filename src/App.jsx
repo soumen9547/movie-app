28bd37598e626707acffd943297be2471ba41bb8
soumen9775 <rakshitsoumen4@gmail.com>
2023-05-28 12:37:21 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-
+import { fetchDataFromApi } from './utils/Api';
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration } from "./store/HomeSlice";
 function App() {
+
+
+  const dispatch = useDispatch();
+  const url = useSelector((state) => state.home);
+  console.log(url)
+
+  useEffect(() => {
+    apiTest();
+  }, []);
+
+  const apiTest = () => {
+    fetchDataFromApi('/movie/popular')
+      .then((res) => {
+        console.log(res);
+        dispatch(getApiConfiguration(res));
+      });
+  }
 
   return (
     <>
-      <div className='app'>App</div>
+      <div className='app'></div>
     </>
   )
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './style.scss';
+export const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
